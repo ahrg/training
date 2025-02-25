a0a3e2142c6f9faac4e2f48e5da602cb02214c9b
ahrg <christofferv3@gmail.com>
2025-02-25 18:57:11 +0100

    t

--- trening/src/App.js
+++ trening/src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import NavBar from './NavBar';
 import WorkoutProgram from './WorkoutProgram.js';
 import Routine from './Routine';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="body">
- 
         <NavBar />
         <Routes>
           <Route path="/" element={<WorkoutProgram />} />
-
           <Route path="/WorkoutProgram" element={<WorkoutProgram />} />
-
           <Route path="/Routine" element={<Routine />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
